b714cc96bb07b25e567e0b16bb645d3610f11fb6
musamuhammad10 <muhammadmusa1004@gmail.com>
2022-07-08 18:33:34 -0400

    Explanatory Data Analysis (EDA) for the trip dataset

--- MidtermProjectScript.R
+++ MidtermProjectScript.R
@@ -1,2 +1,36 @@
 #' MSC2011 Midterm Project
 #' Muhammad Musa, Danni Ma
+
+# Downloading and calling relevant packaages
+install.packages("tidyverse")
+install.packages("funModeling")
+install.packages("Hmisc")
+
+library(funModeling) 
+library(tidyverse) 
+library(Hmisc)
+
+trip <- read.csv("trip.csv")
+trip
+##################################################################################
+# Explanatory data analysis (EDA) for trip
+trip_eda <- function(trip)
+{
+  glimpse(trip)
+  print(status(trip))
+  freq(trip) 
+  print(profiling_num(trip))
+  plot_num(trip)
+  describe(trip)
+}
+
+glimpse(trip)
+status(trip)
+freq(trip)
+
+plot_num(trip)
+trip_prof=profiling_num(trip)
+
+describe(trip)
+
+##################################################################################
