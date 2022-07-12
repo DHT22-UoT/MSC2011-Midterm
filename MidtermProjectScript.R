3ee46e33918c8065933593172a91fdc6e34381f2
Danni Ma <dannima@Dannis-MacBook-Pro.local>
2022-07-12 19:19:52 -0400

    Added coded for duplicated data

--- MidtermProjectScript.R
+++ MidtermProjectScript.R
@@ -40,13 +40,12 @@ summary(trip1$duration) # Maximum duration: 17270400s (199.89 days)
 hist(log10(trip1$duration))
 boxplot(log10(trip1$duration))
 
-  # Remove outliers based on IQR
-trip1q <- quantile(trip1$duration)
-trip1iqr <- IQR(trip1$duration) 
+  # Remove outliers based on IQR (Q3 + 1.5 * IQR or Q1 - 1.5 * IQR)
+trip1q <- quantile(trip1$duration) # Q1 = 345; Q3 = 748
+trip1iqr <- IQR(trip1$duration) # IQR = 403
 
-upperlimit <- trip1q[4] * 1.5
-lowerlimit <- trip1q[2] * 1.5
-lowerlimit <- trip1q[2] - 172.5
+upperlimit <- trip1q[4] + 1.5*trip1iqr
+lowerlimit <- trip1q[2] - 1.5*trip1iqr
 
 trip2 <- trip1 %>%
   filter(duration < upperlimit) %>%
@@ -61,15 +60,28 @@ summary(trip2$duration)
 trip2$start_station_name <- stringr::str_replace(trip2$start_station_name, "Kearny", "Kearney")
 trip2$end_station_name <- stringr::str_replace(trip2$end_station_name, "Kearny", "Kearney")
 
+  # Inconsistency due to duplication 
+length(unique(trip2$start_station_id)) # 70 unique start station ids
+length(unique(trip2$start_station_name)) # 72 unique start station names
+
   # Filter out trips where the start/end station name is not found in the station.csv
 trip3 <- trip2 %>%
   filter(start_station_name %in% station$name) %>%
-  filter(end_station_name %in% station$name)
+  filter(end_station_name %in% station$name) %>%
+  
+  # Filter out trips where the start/end station id is not found in the station.csv
+  filter(start_station_id %in% station$id) %>%
+  filter(end_station_id %in% station$id)
 
   #' Observation: All excluded observations were trips to/from "Broadway at Main" 
   #' or "San Jose Government Center", which are not found in the station.csv file.
-  #' (Note: the "San Jose Government Center" station is assumed different than the 
-  #' "San Jose Civic Center" station)
+  #' In the trip dataset, both the "San Jose Government Center" station and the 
+  #' "Santa Clara County Civic Center" station has a station id of 80. To be 
+  #' consistent with the station dataset, station id 80 corresponds to the 
+  #' "Santa Clara County Civic Center" station, and observations with "San Jose 
+  #' Government Center" station are removed
+table(trip2$start_station_name[trip2$start_station_id == "80"])
+
 
 ##################################################################################
 
