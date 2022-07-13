8f4ad277046327580132782c9e9079baaa7e7e0e
Danni Ma <dannima@Dannis-MacBook-Pro.local>
2022-07-13 02:46:03 -0400

    fixed table join

--- MidtermProjectScript.R
+++ MidtermProjectScript.R
@@ -12,26 +12,38 @@ library(funModeling)
 library(tidyverse) 
 library(Hmisc)
 library("dplyr")
+library(lubridate)
 
-## Reading csv files into dataframe
-trip <- read.csv("trip.csv", na.strings = "")
+## Reading cleaned csv files into dataframe
+trip_clean <- read.csv("trip_clean.csv")
 station <- read.csv("station.csv")
-weather <- read.csv("weather.csv", na.strings = "")
+weather_clean <- read.csv("weather_clean.csv")
 
 ##################################################################################
 
 ### Tables to be joined ###
-trip5 
-weather4
+trip5 <- trip_clean %>%
+  mutate(start_date = as.POSIXct(start_date, format="%m/%d/%Y%H:%M")) %>%
+  mutate(trip_day = wday(start_date, label=TRUE, abbr=FALSE))
+
+weather_clean
 station
 
-### Joining table ###
+### Joining tables ###
 
 # Joining station & trip5 by station id (to get city)
 stationtrip_join <- inner_join(station, trip5, by = c("id" = "start_station_id"))
 
-# Joining stationtrip_joined & weather4 by date and city
-weather_join <- inner_join(stationtrip_join, weather4, by = c("start_date" = "date", "city" = "city"))
+  # Create a new variable to extract date from start_date in stationtrip_join 
+stationtrip_join <- stationtrip_join %>%
+  mutate(date = as.Date(start_date))
+  
+  # Convert variable "date" in weather_clean to class date for consistency
+weather_clean <- weather_clean %>%
+  mutate(date = as.Date(date))
+
+# Joining stationtrip_join & weather_clean by date and city
+weather_join <- inner_join(stationtrip_join, weather_clean, by = c("date" = "date", "city" = "city"))
 
 # Select weather variables from the joined table
 weather_join1 <- weather_join %>%
@@ -41,6 +53,8 @@ weather_join1 <- weather_join %>%
 ### Correlation ###
 install.packages("corrplot")
 library("corrplot")
-corrplot(cor(weather_join1), method = "circle")
+
+cor_matrix <- cor(weather_join1, use="complete.obs")
+corrplot(cor_matrix, method = "circle")
 
 
