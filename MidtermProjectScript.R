289f808316618e3a90a6c11ee9e8d5e2339e7fac
Danni Ma <dannima@Dannis-MacBook-Pro.local>
2022-07-13 00:57:52 -0400

    Added correlation plot

--- MidtermProjectScript.R
+++ MidtermProjectScript.R
@@ -28,13 +28,19 @@ station
 ### Joining table ###
 
 # Joining station & trip5 by station id (to get city)
-stationtrip_joined <- inner_join(station, trip5, by = c("id" = "start_station_id"))
+stationtrip_join <- inner_join(station, trip5, by = c("id" = "start_station_id"))
 
 # Joining stationtrip_joined & weather4 by date and city
-
 weather_join <- inner_join(stationtrip_joined, weather4, by = c("start_date" = "date", "city" = "city"))
 
+# Select weather variables from the joined table
+weather_join1 <- weather_join %>%
+  select(c(duration, max_temperature_f:precipitation_inches))
 
 
+### Correlation ###
+install.packages("corrplot")
+library("corrplot")
+corrplot(cor(weather_join1), method = "circle")
 
 
