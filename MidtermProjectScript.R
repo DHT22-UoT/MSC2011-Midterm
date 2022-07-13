a637672136865c6a122c9cb4d8fc5ed9de018fd3
Danni Ma <dannima@Dannis-MacBook-Pro.local>
2022-07-12 23:34:28 -0400

    Initial commit

--- MidtermProjectScript.R
+++ MidtermProjectScript.R
@@ -1,2 +1,21 @@
 #' MSC2011 Midterm Project
 #' Muhammad Musa, Danni Ma
+
+### Set-up ###
+
+## Downloading and calling relevant packages
+install.packages("tidyverse")
+install.packages("funModeling")
+install.packages("Hmisc")
+install.packages("quantmod")
+library(funModeling) 
+library(tidyverse) 
+library(Hmisc)
+library("dplyr")
+
+## Reading csv files into dataframe
+trip <- read.csv("trip.csv", na.strings = "")
+station <- read.csv("station.csv")
+weather <- read.csv("weather.csv", na.strings = "")
+
+##################################################################################
